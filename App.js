4673232a4734478a00a20583a11c34aeff657acc
shine94info <shineinfosoft94@gmail.com>
2023-10-27 15:35:03 +0530

    first commit

---
+++ App.js
@@ -0,0 +1,10 @@
+import React, {useState} from 'react';
+import SplashScreen from './src/Screens/Splash';
+const App = () => {
+  return (
+    <SplashScreen/>
+    
+  );
+};
+
+export default App;
---
+++ src/screens/splash/index.js
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const SplashScreen = () => {
+  return (
+    <View>
+      <Text>styles</Text>
+    </View>
+  )
+}
+
+export default SplashScreen;
